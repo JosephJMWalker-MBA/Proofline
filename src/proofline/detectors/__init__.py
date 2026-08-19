12609b879b78c9b432a8589f9d5788654e91728d
Joseph Walker <joseph@decisionpointinitiative.com>
2026-08-19 12:29:55 -0400

    Export recurrence variation detector

--- src/proofline/detectors/__init__.py
+++ src/proofline/detectors/__init__.py
@@ -1,5 +1,10 @@
 """Deterministic Proofline observation detectors."""
 
+from .recurrence_variation import (
+    RecurrenceVariationCandidate,
+    RecurrenceVariationDecision,
+    evaluate_recurrence_variation,
+)
 from .version_diff import (
     ArithmeticRelation,
     EvidenceUnitDiff,
@@ -11,7 +16,10 @@ from .version_diff import (
 __all__ = [
     "ArithmeticRelation",
     "EvidenceUnitDiff",
+    "RecurrenceVariationCandidate",
+    "RecurrenceVariationDecision",
     "VersionDiffResult",
     "build_version_change_observation",
     "compare_artifact_versions",
+    "evaluate_recurrence_variation",
 ]
